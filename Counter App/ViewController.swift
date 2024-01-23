18569ece8728e83214dbabe36e691385ce164526
slenbder <152342422+slenbder@users.noreply.github.com>
2024-01-23 21:47:49 +0300

    New Logo

--- Counter App/ViewController.swift
+++ Counter App/ViewController.swift
@@ -1,19 +1,67 @@
-//
-//  ViewController.swift
-//  Counter App
-//
-//  Created by Кирилл Марьясов on 21.01.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    private var counterView: Int = 0
+    private var logTexView: String = "История изменений:\n"
+    private func getCurrentDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        return dateFormatter.string(from: Date())
+    }
+    
+     override func viewDidLoad() {
+         super.viewDidLoad()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterLabel.text = "\(counterView)"
+        changesHistory.text = logTexView
+        changesHistory.isEditable = false
     }
 
+    private func plus() {
+        counterView += 1
+        counterLabel.text = "\(counterView)"
 
+        let currentDate = getCurrentDate()
+        logTexView += "\(currentDate): Значение изменено на +1\n"
+        changesHistory.text = logTexView
+    }
+
+    private func minus() {
+        if counterView != 0 {
+            counterView -= 1
+            counterLabel.text = "\(counterView)"
+
+            let currentDate = getCurrentDate()
+            logTexView += "\(currentDate): Значение изменено на -1\n"
+            changesHistory.text = logTexView
+        } else {
+            let currentDate = getCurrentDate()
+            logTexView += "\(currentDate): Попытка уменьшить значение счетчика ниже 0\n"
+            changesHistory.text = logTexView
+        }
+    }
+
+    private func reset() {
+        counterView = 0
+        counterLabel.text = "\(counterView)"
+
+        let currentDate = getCurrentDate()
+        logTexView += "\(currentDate): Значение сброшено\n"
+        changesHistory.text = logTexView
+    }
+
+    @IBOutlet weak var changesHistory: UITextView!
+    @IBOutlet weak var counterLabel: UILabel!
+
+    @IBAction func plusButton(_ sender: Any) {
+        plus()
+    }
+
+    @IBAction func minusButton(_ sender: Any) {
+        minus()
+    }
+
+    @IBAction func resetButton(_ sender: Any) {
+        reset()
+    }
 }
-
